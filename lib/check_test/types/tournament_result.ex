14782b2e09a52870a4500b6553d7b95973857d16
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-06-09 15:52:50 +0300

    removed useless types

--- lib/check_test/types/tournament_result.ex
+++
@@ -1,6 +0,0 @@
-defmodule CheckTest.Type.TournamentResult do  
-
-  @derive [Poison.Encoder]
-  defstruct tournamentId: "",
-            winners: []
-end
--- lib/check_test/types/winner.ex
+++
@@ -1,5 +0,0 @@
-defmodule CheckTest.Type.Winner do  
-
-  @derive [Poison.Encoder]
-  defstruct [:playerId, :prize]
-end
